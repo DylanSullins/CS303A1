d45b92271df810b2bc1681314ab7d21be2e56b47
= <sullins.dylan@gmail.com>
2024-08-31 09:11:12 -0500

    Added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@
 
 int main(int argc, char* argv[])
 {
-	// Initialize Array
-	// Read from file
-	// Do user commands
-	//
+	// Check arguments for proper amount
+	// Arguments past the necessary three will be ignored
 	if (argc < 3) 
 	{
 		std::cout << "Error: Not enough arguments provided to the program. " << argc - 1 
@@ -17,31 +15,43 @@ int main(int argc, char* argv[])
 	}
 
 	std::ifstream fin;
+	// Get size from command line arguments
 	std::stringstream ss(argv[2]);
 	int size;
 	ss >> size;
+
+	// Validate size
 	if (size <= 0)
 	{
 		std::cout << size << " is not a valid size for the array." << std::endl;
 		return -1;
 	}
 
+	// Open File and Validate
 	fin.open(argv[1]);
 	if (!fin.is_open()) {
 		std::cout << "Error: File " << argv[1] << " not found." << std::endl;
 		return -1;
 	}
+	// Initialize array 
 	int arr[size] {};
+
+	// Store input values in array
 	processInputFile(arr, size, fin);
+
+	// Close file
 	fin.close();
+
+	// Get inpput for displaying
 	int columns = getInt("Enter number of columns to display: ");
 	printArray(arr, size, columns);
 	std::cout << std::endl;
 
-
+	// Main Loop
 	while (true)
 	{
 		printMenu();
+		// processInput returns true if input is 'q'/'Q'
 		if(processInput(arr, size)) break;
 	}
 
